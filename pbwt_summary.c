c4f4ea15d64a757572fabfeb6ce7e4e0dfa802b1
Daniel Garrigan <popgendad@gmail.com>
2019-12-09 20:54:45 -0800

    Summary function now outputs total recombination distance

--- pbwt_summary.c
+++ pbwt_summary.c
@@ -32,6 +32,7 @@ int pbwt_summary(cmd_t *c)
     /* Print summary report */
     printf("Number of samples:\t%zu\n", b->nsam);
     printf("Number of sites:\t%zu\n", b->nsite);
+    printf("Total recombination distance:\t%1.5lf\n", b->cm[b->nsite-1] - b->cm[0]);
     printf("Size of compressed data:\t%zu\n", orig_size);
     printf("Size of uncompressed data:\t%zu\n", b->datasize);
 
